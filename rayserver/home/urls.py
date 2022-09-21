f893ad3ceda9c58d007635b6578fe26c31604e0a
최대열 <86054791+zakigaebal@users.noreply.github.com>
2022-09-21 16:16:17 +0900

    메인페이지수정중

--- rayserver/home/urls.py
+++ rayserver/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, home_view
 
 app_name = "home"
 
 urlpatterns = [    
 # 홈페이지
     path('', index, name=''),
+    path('home', home_view, name='home')
 ]
--- rayserver/home/views.py
+++ rayserver/home/views.py
@@ -9,3 +9,12 @@ def index(request):
     context['m_id'] = request.session.get('m_id', '')
     context['m_name'] = request.session.get('m_name', '')
     return render(request, 'home/index.html', context)
+
+
+@csrf_exempt
+def home_view(request):
+    context = {}
+    # m_id 세션변수 값이 없다면 '' 을 넣어라
+    context['m_id'] = request.session.get('m_id', '')
+    context['m_name'] = request.session.get('m_name', '')
+    return render(request, 'home/index2.html', context)
