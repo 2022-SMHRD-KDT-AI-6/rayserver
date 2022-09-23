3276b12f120a3b186287edc7214827eed63a9646
Subin Lee <joyulee77@gmail.com>
2022-09-23 17:22:26 +0900

    로그인페이지 수정!

--- rayserver/member/views.py
+++ rayserver/member/views.py
@@ -12,12 +12,12 @@ def index(request):
     # m_id 세션변수 값이 없다면 '' 을 넣어라
     context['m_id'] = request.session.get('m_id', '')
     context['m_name'] = request.session.get('m_name', '')
-    return render(request, 'member/index.html', context)
+    return render(request, 'home/index.html', context)
 
 @csrf_exempt
 def member_reg(request):
     if request.method == "GET":
-        return render(request, 'member/member_reg.html')
+        return render(request, 'home/pages/samples/registern.html')
     elif request.method == "POST":
         context = {}
         mem_id = request.POST["mem_id"]
@@ -33,7 +33,7 @@ def member_reg(request):
         rs = Members.objects.filter(mem_id=mem_id)
         if rs.exists():
             context['message'] = mem_id + "가 중복됩니다."
-            return render(request, 'member/member_reg.html', context)
+            return render(request, 'home/pages/samples/registern.html', context)
 
         else:
             Members.objects.create(
@@ -45,7 +45,7 @@ def member_reg(request):
 @csrf_exempt
 def member_login(request):
     if request.method == "GET":
-        return render(request, 'member/login.html')
+        return render(request, 'home/pages/samples/loginn.html')
     elif request.method == "POST":
         context = {}
 
@@ -61,7 +61,7 @@ def member_login(request):
         #if rs.exists():
         if rs is None:
             print(Response(dict(msg="해당 ID의 사용자가 없습니다.")))
-            return render(request, "member/login.html", status=401)
+            return render(request, "home/pages/samples/loginn.html", status=401)
         if check_password(mem_pw, rs.mem_pw):
             # OK - 로그인
             request.session['m_id'] = mem_id
@@ -72,7 +72,7 @@ def member_login(request):
             return render(request, 'home/index.html', context)
         else:
             context['message'] = "로그인 실패. 패스워드 불일치\\n\\n확인하신 후 다시 시도해 주십시오."
-            return render(request, 'member/login.html', context)
+            return render(request, 'home/pages/samples/loginn.html', context)
 @csrf_exempt
 def member_logout(request):
     request.session.flush()
