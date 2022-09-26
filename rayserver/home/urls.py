df5cba2814f480bfa9a04f599f66978d5e6640a1
Subin Lee <joyulee77@gmail.com>
2022-09-26 17:26:00 +0900

    index랑 프로젝트 소개 페이지

--- rayserver/home/urls.py
+++ rayserver/home/urls.py
@@ -1,10 +1,14 @@
+from unicodedata import name
 from django.urls import path
-from .views import index, home_view
+from .views import index, home_view, introcom_view, introrey_view, introteam_view
 
 app_name = "home"
 
 urlpatterns = [    
 # 홈페이지
     path('', index, name=''),
-    path('home', home_view, name='home')
+    path('home', home_view, name='home'),
+    path('introcom', introcom_view, name='introcom'),
+    path('introrey', introrey_view, name='introrey'),
+    path('introteam', introteam_view, name='introteam')
 ]
--- rayserver/home/views.py
+++ rayserver/home/views.py
@@ -18,3 +18,21 @@ def home_view(request):
     context['m_id'] = request.session.get('m_id', '')
     context['m_name'] = request.session.get('m_name', '')
     return render(request, 'home/index2.html', context)
+
+def introcom_view(request):
+    context = {}
+    context['m_id'] = request.session.get('m_id', '')
+    context['m_name'] = request.session.get('m_name', '')
+    return render(request, 'home/pages/samples/뉴로젠.html', context)
+
+def introrey_view(request):
+    context = {}
+    context['m_id'] = request.session.get('m_id', '')
+    context['m_name'] = request.session.get('m_name', '')
+    return render(request, 'home/pages/samples/프로젝트소개.html', context)
+
+def introteam_view(request):
+    context = {}
+    context['m_id'] = request.session.get('m_id', '')
+    context['m_name'] = request.session.get('m_name', '')
+    return render(request, 'home/pages/samples/기억할레이.html', context)
